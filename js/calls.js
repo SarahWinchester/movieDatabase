52f1db9f3943378ab6d8c8e75dd06de4261f1ebe
SarahWinchester <indira_ruli@hotmail.com>
2018-08-27 06:05:32 -0500

    collapsible done, image not responsive yet

--- js/calls.js
+++ js/calls.js
@@ -27,60 +27,63 @@ function bringImage(data) {
         let votes= data.results[i].vote_average;
         let poster= data.results[i].poster_path;
         let posterURl = "https://image.tmdb.org/t/p/original"+ poster;
-
-            let template =`<div class="collapsible  container">
-            <img class="col-4 responsive-img" src="{{minImg}}">
-            <span class="col-6">{{titles}}</span>
-            <span class="col-2 " >Puntuación {{vote}}/100</span>
-            </div>
-            <div class="content container">
-            <img class="col-4" src="{{poster}}" alt="">
-            <div class="col-5">
-            <span>{{title}}</span>
-            <div>{{overview}}</div>
-            </div>
-            <div class=" col-3">
-            <span>{{releaseDate}}</span>
-            <span>{{originallenguage}}</span>
-            </div>
-            </div>`
-            
-            let newTemplate= template.replace("{{minImg}}", backdropUrl)
-            .replace("{{vote}}",votes)
-            .replace("{{poster}}", posterURl)
-            .replace("{{titles}}", originalTitle)
-            .replace("{{overview}}", resume) 
-            .replace("{{releaseDate}}", release)
-            .replace("{{title}}", title)
-            .replace("{{originallenguage}}", lenguage);
-            $("#containCollaps").append(newTemplate);
-            
-            
-        }
         
+        let template =`<div class="collapsible  container">
+       
+        <img class="col--4 responsive-img" src="{{minImg}}">
+        <span class="col-6">{{titles}}</span>
+        <span class="col-2 " >Puntuación {{vote}}/100</span>
+        </div>
+        <div class="content container">
+        <div class="row">
+        <img class="col-5 responsive-image" src="{{poster}}" alt="">
+        <div class="col-4">
+        <span>{{title}}</span>
+        <div>{{overview}}</div>
+        </div>
+        <div class=" col-3">
+        <span>{{releaseDate}}</span>
+        <span>{{originallenguage}}</span>
+        </div>
         
-        
+        </div>
+        </div>`
+        let newTemplate= template.replace("{{minImg}}", backdropUrl)
+        .replace("{{vote}}",votes)
+        .replace("{{poster}}", posterURl)
+        .replace("{{titles}}", originalTitle)
+        .replace("{{overview}}", resume) 
+        .replace("{{releaseDate}}", release)
+        .replace("{{title}}", title)
+        .replace("{{originallenguage}}", lenguage);
+        $("#containCollaps").append(newTemplate);
         
         
     }
     
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+    
+    
+    
+}
+
 
 
 // collapsable 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
 
 bringMovies(key)
