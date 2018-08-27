0e0dd0d9c5c75e0a65f7cad6f9f0dc576f4b2ac7
SarahWinchester <indira_ruli@hotmail.com>
2018-08-26 21:46:31 -0500

    all data in console

--- js/calls.js
+++ js/calls.js
@@ -1,7 +1,7 @@
 const key= "08181a42528d77449ac34296a1328506";
 
 function bringMovies (key){
-
+    
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
     .then((response) => {
         // console.log(response);
@@ -15,6 +15,20 @@ function bringMovies (key){
     })
 }
 function bringImage(data) {
-    let id= data;
+    // console.log(data.results);
+    for (let i = 0; i < (data.results).length; i++) {
+        let dataBackDrop = data.results[i].backdrop_path;
+        let backdropUrl = "https://image.tmdb.org/t/p/w500/"+ dataBackDrop;
+        let lenguage= data.results[i].original_language;
+        let originalTitle = data.results[i].original_title;
+        let title= data.results[i].title;
+        let resume= data.results[i].overview;        
+        let release= data.results[i].release_date;
+        let votes= data.results[i].vote_average;
+        let poster= data.results[i].poster_path;
+        let posterURl = "https://image.tmdb.org/t/p/original"+ poster;
+        
+        }
+    
 }
 bringMovies(key)
